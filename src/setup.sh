1aba26f36411db250f8c632322bac898efecd3fb
sking115422 <spencerd.king@gmail.com>
2021-12-01 00:14:58 -0500

    fixed setup.sh file to make auto make folders

--- src/setup.sh
+++ src/setup.sh
@@ -1,12 +1,16 @@
 rm -r venv
 
-pip install virtualenv
+mkdir ../DistShared
+
+mkdir ../Downloads
+
+pip3 install virtualenv
 
 virtualenv venv
 
 source venv/bin/activate
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 echo ""
 echo "SETUP COMPLETE"
