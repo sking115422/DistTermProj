214bf3c6256fc0ccc04f9e3bb68f164ef47ec2f6
sking115422 <63437172+sking115422@users.noreply.github.com>
2021-12-05 02:36:19 -0500

    Update update_time.py

--- src/update_time.py
+++ src/update_time.py
@@ -1,42 +1,54 @@
 
 
+#IMPORT STATEMENTS
+
 from datetime import datetime as dt
 import time as t
 import mysql.connector
 from pytz import timezone
 
-t.sleep (3)
 
+
+#DATABASE TIMESTAMP UPDATER SCRIPT
+
+#Allows time for the peer server to push files to the central server (MySQL DB) before updating timestamps
+t.sleep (2)
+
+#Getting current machine IP address from file where it is stored
 with open('machine_ip.txt') as f:
     machine_ip = str(f.readlines()[0])
 
+#Connecting to MySQL database
+
+#***YOU WILL NEED TO UPDATE THESE VALUES FOR YOUR OWN CENTRAL DATABASE***
 db = mysql.connector.connect(
-    host='173.230.133.41',
-    user='user',
-    password=''
+    host='173.230.133.41',    #UPDATE
+    user='user',    #UPDATE
+    password=''    #UPDATE
 )
 
 mycursor = db.cursor()
 
+
 print ("update_time.py is running")
 
+#Loop to continually update time stamp until program is killed or machine is shutdown
 while True:
     
+    #Specifying easten timezone and taking current time
+    #Putting current time in compatible timestamp format
     now = None
     tz = timezone('EST')
     now = dt.now(tz)
     now = now.strftime("%Y-%m-%d %H:%M:%S.%f")
     
+    #Updating all timestamps associate with our machine's IP
     mycursor.execute ("USE CDS;")
-    
     cmd = "UPDATE peer_list SET time_stamp = '" + now +  "' WHERE IP = '" + machine_ip + "';"
-    # print (cmd)
-    
     mycursor.execute (cmd)
-    
     print("Timestamp Updated:   " + now)
-    
     db.commit()
     
+    #Waiting 15 seconds to update again
     t.sleep(15)
 
